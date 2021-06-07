0f9c5b81b51d9d80374e9e6b1997037f4e651f9b
tino.t <tino.thamjarat@unity3d.com>
2021-06-07 16:54:27 +0300

    Do not convert img
    Remove img conversion from PDF to HTML cmd and add rake task
    for running parser

--- lib/pdf_converter.rb
+++ lib/pdf_converter.rb
@@ -8,7 +8,7 @@ class PDFConverter
   # +input_path+:: a path string for pdf input from the root dir. Default: 'budget-pdf'
   # +output_path+:: a path string to save html output from the root dir. Default: 'budget-html'
   # +output_suffix+:: a string to add as suffix on the output filename. Default: ''
-  def extract(start_page, end_page, output_suffix: '', input_path: 'budget-pdf', output_path: 'lib/budget-html', extract_all: false)
+  def extract(start_page, end_page, output_suffix: '', input_path: 'budget-pdf', output_path: 'lib/budget-html')
     proj_root_path, pdf_dir = gen_dirs(input_path, output_path)
 
     Dir.foreach(pdf_dir) do |filename|
@@ -24,9 +24,10 @@ class PDFConverter
     puts 'done'
   end
 
-  def extract_all(input_path: 'budget-pdf', output_path: 'lib/budget-html')
+  def extract_all(input_path: 'budget-pdf', output_path: 'lib/budget-html', output_suffix: '')
     proj_root_path, pdf_dir = gen_dirs(input_path, output_path)
-    system extract_all(input)
+    output_filename = gen_output_filename('8', output_suffix)
+    system extract_all_cmd(proj_root_path, File.join(input_path, '8.pdf'), File.join(output_path, output_filename))
   end
 
   # returns fully expanded [proj_root_path, input_path, output_path]
@@ -44,10 +45,10 @@ class PDFConverter
   end
 
   private def extract_cmd(start_page, end_page, proj_path, input_path, output_filename)
-    "docker run -ti --rm -v #{proj_path}:/pdf pdf2htmlex/pdf2htmlex:#{IMAGE_VERSION} -f #{start_page} -l #{end_page} --tounicode 1 --optimize-text 1 --space-as-offset 0 --process-outline 0 #{input_path} #{output_filename}"
+    "docker run -ti --rm -v #{proj_path}:/pdf pdf2htmlex/pdf2htmlex:#{IMAGE_VERSION} -f #{start_page} -l #{end_page} --tounicode 1 --optimize-text 1 --space-as-offset 0 --process-outline 0 --process-nontext 0 #{input_path} #{output_filename}"
   end
 
   private def extract_all_cmd(proj_path, input_path, output_filename)
-    "docker run -ti --rm -v #{proj_path}:/pdf pdf2htmlex/pdf2htmlex:#{IMAGE_VERSION} --tounicode 1 --optimize-text 1 --space-as-offset 0 --process-outline 0 #{input_path} #{output_filename}"
+    "docker run -ti --rm -v #{proj_path}:/pdf pdf2htmlex/pdf2htmlex:#{IMAGE_VERSION} --tounicode 1 --optimize-text 1 --space-as-offset 0 --process-outline 0 --process-nontext 0 #{input_path} #{output_filename}"
   end
 end
--- lib/pdf_converter_test.rb
+++ lib/pdf_converter_test.rb
@@ -5,24 +5,24 @@ class TestPDFConverter < Minitest::Test
   HTML_OUTPUT_PATH = 'budget-html'
   def test_extract_only_cover
     # cleanup the folder
-    delete_all_budget_html_files
+    delete_all_budget_html_cover_files
+
+    PDFConverter.new.extract 3, 3, output_suffix: 'cover'
 
     output_path = File.join(File.dirname(__FILE__), HTML_OUTPUT_PATH)
-    PDFConverter.new.extract 3, 3, output_suffix: 'cover'
     assert_equal 28, Dir[File.join(output_path, '**', '*-cover.html')].count { |file| File.file?(file) } 
   end
 
   def test_extract_all
-    # output_path = File.join(File.dirname(__FILE__), '../budget-html')
-    # PDFConverter.new.extract 3, 3
-    # assert_equal 28, Dir[File.join(output_path, '**', '*-full.html')].count { |file| File.file?(file) } 
-    assert_equal false, true
+    PDFConverter.new.extract_all(output_suffix: 'full')
+    
+    output_path = File.join(File.dirname(__FILE__), HTML_OUTPUT_PATH)
+    assert_equal 28, Dir[File.join(output_path, '**', '*-full.html')].count { |file| File.file?(file) }
   end
 
-  private def delete_all_budget_html_files
-    Dir.foreach(HTML_OUTPUT_PATH) do |f|
-      fn = File.join(HTML_OUTPUT_PATH, f)
-      File.delete(fn) if f != '.' && f != '..'
+  private def delete_all_budget_html_cover_files
+    Dir.glob(File.join(HTML_OUTPUT_PATH, '**', '*-cover.html')).each do |f|
+      File.delete(f) if f != '.' && f != '..'
     end
   end
 end
